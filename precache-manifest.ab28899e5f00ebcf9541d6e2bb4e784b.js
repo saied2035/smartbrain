d55fcfb0b113693f96ad8c940a6625a2878a4fd4
Saied Gaffer <saied2421998@gmail.com>
2020-09-28 14:23:56 +0200

    Updates

---
+++ precache-manifest.ab28899e5f00ebcf9541d6e2bb4e784b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bda4ff79fef70246cf0442f38d44d8",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "e2f1fdca68680aa14702",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7b42b8205fc0f8d56320",
+    "url": "/facerecognition/static/css/main.5e75fb6a.chunk.css"
+  },
+  {
+    "revision": "e2f1fdca68680aa14702",
+    "url": "/facerecognition/static/js/2.5c804214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.5c804214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b42b8205fc0f8d56320",
+    "url": "/facerecognition/static/js/main.09e5f088.chunk.js"
+  },
+  {
+    "revision": "634e179f90a87c513456",
+    "url": "/facerecognition/static/js/runtime-main.c0267974.js"
+  },
+  {
+    "revision": "69b0cd2198ac5f938a845dedd9e596de",
+    "url": "/facerecognition/static/media/VampireBite-qZK3d.69b0cd21.woff"
+  },
+  {
+    "revision": "489fefce3c2997914495535dedb38f5d",
+    "url": "/facerecognition/static/media/VampireBiteSolid-AL0wD.489fefce.woff"
+  }
+]);
