00d549f934b67bb87bdc5a47abc2b6a27d60afbf
Saied Gaffer <saied2421998@gmail.com>
2020-10-07 18:03:45 +0200

    edit the css again

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './css/ImageLinkForm.css'
  const ImageLinkForm = ({onSearchChange,onButtonClick,error}) => {
   if(error) {
   	  return (
-	  <div className='center mt3 w-60 shadow-5 pa3 background ba b--light-green flex-wrap'>
+	  <div style={{width:'70vw', height : 'auto'}} className='center mt3 shadow-5 pa3 background ba b--light-green flex-wrap'>
 	  <input onChange={onSearchChange} className='shadow-5 b--red bg-light-green w-80' type='text'/>
 	  <button onClick={onButtonClick} className='ml2 bg-light-green pointer f3 grow'>Detect</button>
 	  <strong className="shadow-4 mt1 f4 navy">{`you didn't enter a vaild image format`}</strong>
@@ -12,7 +12,7 @@ import './css/ImageLinkForm.css'
   }
 
   return (
-	  <div className='center mt3 w-60 shadow-5 pa3 background ba b--light-green '>
+	  <div style={{width:'70vw', height:'auto'}} className='center mt3 shadow-5 pa3 background ba b--light-green '>
 	  <input onChange={onSearchChange} className='w-80 bg-light-green' type='text' 
 	   placeholder='Enter image URL (jpg,jpeg or png only)'/>
 	  <button onClick={onButtonClick} className='ml2 pa1 bg-light-green pointer f3 grow'>Detect</button>
